64dd0c9b4a10a997246ce55a08fc1ea8b786aac6
LeDuongNhat <nhatnhatnhatle@gmail.com>
2024-01-27 19:06:52 +0700

    commit-1

---
+++ validator.js
@@ -0,0 +1,83 @@
+function Validator(options) {
+  var selectorRules = {};
+  function validate(inputElement, rule, check) {
+    var errorElement = inputElement.parentElement.querySelector(
+      options.errorSelector
+    );
+
+    var errorMessage = rule.test(inputElement.value);
+    if (check === true) {
+      if (errorMessage) {
+        errorElement.innerText = errorMessage;
+        inputElement.parentElement.classList.add("invalid");
+      } else {
+        errorElement.innerText = "";
+        inputElement.parentElement.classList.remove("invalid");
+      }
+    } else {
+      errorElement.innerText = "";
+      inputElement.parentElement.classList.remove("invalid");
+    }
+  }
+
+  var formElement = document.querySelector(options.form);
+
+  if (formElement) {
+    options.rules.forEach(function (rule) {
+      var inputElement = formElement.querySelector(rule.selector);
+
+      if (inputElement) {
+        // Xử lý trường hợp blur khởi input
+        inputElement.onblur = function () {
+          validate(inputElement, rule, true);
+        };
+
+        // Xử lý mỗi khi người dùng nhập vào input
+        inputElement.oninput = function () {
+          validate(inputElement, rule, false);
+        };
+      }
+    });
+  }
+}
+
+Validator.isRequired = function (selector, message) {
+  return {
+    selector: selector,
+    test: function (value) {
+      return value.trim() ? undefined : message || "Vui lòng nhập trường này";
+    },
+  };
+};
+
+Validator.isEmail = function (selector, message) {
+  return {
+    selector: selector,
+    test: function (value) {
+      var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+      return regex.test(value) ? undefined : message || "Vui lòng nhập email";
+    },
+  };
+};
+
+Validator.minLength = function (selector, min, message) {
+  return {
+    selector: selector,
+    test: function (value) {
+      return value.length >= min
+        ? undefined
+        : message || `Vui lòng nhập tối thiểu ${min} kí tự`;
+    },
+  };
+};
+
+Validator.isConfirmed = function (selector, getConfirmValue, message) {
+  return {
+    selector: selector,
+    test: function (value) {
+      return value === getConfirmValue()
+        ? undefined
+        : message || "Giá trị nhập vào không chính xác";
+    },
+  };
+};
